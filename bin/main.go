dfde3c1b652662a898378e4fc345ab7b79a1e7ed
Haris Akbar <harisakbar04@gmail.com>
2023-06-16 17:11:13 +0700

    fixing webrtc and websocket

--- bin/main.go
+++ bin/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"sync"
 
 	"github.com/gorilla/websocket"
 	"github.com/pion/mediadevices"
@@ -19,7 +20,30 @@ import (
 	_ "github.com/pion/mediadevices/pkg/driver/camera"
 )
 
-var peerConnection *webrtc.PeerConnection
+var (
+	peerConnection  *webrtc.PeerConnection
+	isDriverUsed    = false
+	localTracks     []mediadevices.Track
+	peerConnections []peerConnectionState
+)
+
+type peerConnectionState struct {
+	peerConnection *webrtc.PeerConnection
+	websocket      *threadSafeWriter
+}
+
+// Helper to make Gorilla Websocket threadsafe
+type threadSafeWriter struct {
+	*websocket.Conn
+	sync.Mutex
+}
+
+func (t *threadSafeWriter) WriteJSON(v interface{}) error {
+	t.Lock()
+	defer t.Unlock()
+
+	return t.Conn.WriteJSON(v)
+}
 
 func HTTPServer() {
 	port := flag.Int("port", 8080, "http server port")
@@ -51,95 +75,97 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 	}
 
 	upgrader := websocket.Upgrader{}
-	conn, err := upgrader.Upgrade(w, r, nil)
+	unsafeConn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		panic(err)
 	}
 
-	go func() {
-		defer conn.Close()
+	c := &threadSafeWriter{unsafeConn, sync.Mutex{}}
 
-		for {
-			_, msg, err := conn.ReadMessage()
-			if err != nil {
+	defer c.Close()
+
+	for {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
 				log.Println(err)
 			}
+			break
+		}
 
-			msgRes := BaseMessage{}
-			switch json.Unmarshal(msg, &msgRes); msgRes.Topic {
-			case "offer":
-				if peerConnection != nil {
+		msgRes := BaseMessage{}
+
+		switch json.Unmarshal(msg, &msgRes); msgRes.Topic {
+		case "offer":
+			config := webrtc.Configuration{
+				ICEServers: []webrtc.ICEServer{
+					{
+						URLs: []string{"stun:stun.l.google.com:19302", "stun:stun.services.mozilla.com"},
+					},
+				},
+			}
+
+			// Create a new RTCPeerConnection
+			vp8Params, err := vpx.NewVP8Params()
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(vp8Params.RTPCodec())
+			vp8Params.BitRate = 1000_000 // 500kbps
+
+			codecSelector := mediadevices.NewCodecSelector(
+				mediadevices.WithVideoEncoders(&vp8Params),
+			)
+
+			mediaEngine := webrtc.MediaEngine{}
+			codecSelector.Populate(&mediaEngine)
+			api := webrtc.NewAPI(webrtc.WithMediaEngine(&mediaEngine))
+			peerConnection, err = api.NewPeerConnection(config)
+			if err != nil {
+				panic(err)
+			}
+
+			// Set the handler for ICE connection state
+			// This will notify you when the peer has connected/disconnected
+			peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
+				fmt.Printf("Connection State has changed %s \n", connectionState.String())
+			})
+
+			peerConnection.OnICECandidate(func(i *webrtc.ICECandidate) {
+				if i == nil {
 					return
 				}
 
-				config := webrtc.Configuration{
-					ICEServers: []webrtc.ICEServer{
-						{
-							URLs: []string{"stun:stun.l.google.com:19302", "stun:stun.services.mozilla.com"},
-						},
-					},
-				}
-
-				// Create a new RTCPeerConnection
-				openh264Params, err := vpx.NewVP8Params()
+				candidateString, err := json.Marshal(i.ToJSON())
 				if err != nil {
-					panic(err)
-				}
-				fmt.Println(openh264Params.RTPCodec())
-				openh264Params.BitRate = 100_000 // 500kbps
-
-				codecSelector := mediadevices.NewCodecSelector(
-					mediadevices.WithVideoEncoders(&openh264Params),
-				)
-
-				mediaEngine := webrtc.MediaEngine{}
-				codecSelector.Populate(&mediaEngine)
-				api := webrtc.NewAPI(webrtc.WithMediaEngine(&mediaEngine))
-				peerConnection, err = api.NewPeerConnection(config)
-				if err != nil {
-					panic(err)
+					log.Println(err)
+					return
 				}
 
-				// Set the handler for ICE connection state
-				// This will notify you when the peer has connected/disconnected
-				peerConnection.OnICEConnectionStateChange(func(connectionState webrtc.ICEConnectionState) {
-					fmt.Printf("Connection State has changed %s \n", connectionState.String())
-				})
-
-				peerConnection.OnICECandidate(func(candidate *webrtc.ICECandidate) {
-					if candidate != nil {
-						w, err := conn.NextWriter(websocket.TextMessage)
-						if err != nil {
-							panic(err)
-						}
-						candidateMsg := BaseMessage{}
-						candidateInit := webrtc.ICECandidateInit{}
-						candidateInit.Candidate = candidate.ToJSON().Candidate
-						candidateInit.SDPMLineIndex = candidate.ToJSON().SDPMLineIndex
-						candidateInit.SDPMid = candidate.ToJSON().SDPMid
-						candidateInit.UsernameFragment = candidate.ToJSON().UsernameFragment
-
-						candidateByte, _ := json.Marshal(candidateInit)
-						candidateMsg.Topic = "candidate"
-						candidateMsg.Data = string(candidateByte)
-						msgByte, _ := json.Marshal(candidateMsg)
-						w.Write(msgByte)
-					}
-				})
-
-				s, err := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
-					Video: func(c *mediadevices.MediaTrackConstraints) {
-						c.Height = prop.Int(480)
-						c.Width = prop.Int(640)
-					},
-					Codec: codecSelector,
-				})
-				if err != nil {
-					panic(err)
+				if writeErr := c.WriteJSON(&BaseMessage{
+					Topic: "candidate",
+					Data:  string(candidateString),
+				}); writeErr != nil {
+					log.Println(writeErr)
 				}
+			})
 
+			s, err := mediadevices.GetUserMedia(mediadevices.MediaStreamConstraints{
+				Video: func(c *mediadevices.MediaTrackConstraints) {
+					c.Height = prop.Int(480)
+					c.Width = prop.Int(640)
+				},
+				Codec: codecSelector,
+			})
+			if err != nil {
+				isDriverUsed = true
+				log.Println(err)
+			}
+
+			if !isDriverUsed {
 				for _, track := range s.GetTracks() {
 					fmt.Println(track.Kind())
+					localTracks = append(localTracks, track)
 					track.OnEnded(func(err error) {
 						fmt.Printf("Track (ID: %s) ended with error: %v\n",
 							track.ID(), err)
@@ -150,60 +176,64 @@ func serveWs(w http.ResponseWriter, r *http.Request) {
 							Direction: webrtc.RTPTransceiverDirectionSendonly,
 						},
 					)
+
 					if err != nil {
 						panic(err)
 					}
 				}
+			} else {
+				_, err = peerConnection.AddTransceiverFromTrack(localTracks[0],
+					webrtc.RTPTransceiverInit{
+						Direction: webrtc.RTPTransceiverDirectionSendonly,
+					},
+				)
+			}
 
-				// Set the remote SessionDescription
-				offer := webrtc.SessionDescription{}
-				err = json.Unmarshal([]byte(msgRes.Data), &offer)
-				log.Println(offer)
-				if err != nil {
-					log.Println(err)
-				}
-				err = peerConnection.SetRemoteDescription(offer)
-				if err != nil {
-					panic(err)
-				}
+			// Set the remote SessionDescription
+			offer := webrtc.SessionDescription{}
+			err = json.Unmarshal([]byte(msgRes.Data), &offer)
+			log.Println(offer)
+			if err != nil {
+				log.Println(err)
+			}
+			err = peerConnection.SetRemoteDescription(offer)
+			if err != nil {
+				panic(err)
+			}
 
-				// Create an answer
-				answer, err := peerConnection.CreateAnswer(nil)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println("ctx: create answer")
-				// Sets the LocalDescription, and starts our UDP listeners
-				err = peerConnection.SetLocalDescription(answer)
-				if err != nil {
-					panic(err)
-				}
-				fmt.Println(answer.SDP)
+			// Create an answer
+			answer, err := peerConnection.CreateAnswer(nil)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println("ctx: create answer")
+			// Sets the LocalDescription, and starts our UDP listeners
+			err = peerConnection.SetLocalDescription(answer)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Println(answer.SDP)
 
-				answerRes := BaseMessage{}
-				answerRes.Topic = "answer"
-				answerSD, _ := json.Marshal(answer)
-				answerRes.Data = string(answerSD)
-				res, _ := json.Marshal(answerRes)
+			answerSD, _ := json.Marshal(answer)
 
-				w, err := conn.NextWriter(websocket.TextMessage)
-				if err != nil {
-					panic(err)
-				}
+			if writeErr := c.WriteJSON(&BaseMessage{
+				Topic: "answer",
+				Data:  string(answerSD),
+			}); writeErr != nil {
+				log.Println(writeErr)
+			}
 
-				w.Write(res)
-
-			case "candidate":
-				log.Println("add candidate")
-				candidate := webrtc.ICECandidateInit{}
-				json.Unmarshal([]byte(msgRes.Data), &candidate)
-				err := peerConnection.AddICECandidate(candidate)
-				if err != nil {
-					log.Println(err)
-				}
+		case "candidate":
+			log.Println("add candidate")
+			candidate := webrtc.ICECandidateInit{}
+			json.Unmarshal([]byte(msgRes.Data), &candidate)
+			err := peerConnection.AddICECandidate(candidate)
+			if err != nil {
+				log.Println(err)
 			}
 		}
-	}()
+	}
+
 }
 
 func main() {
